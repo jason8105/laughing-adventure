7704fd190e36ea64d9862fcda2b55c0edd71c759
jason8105 <jasondsilva8197@gmail.com>
2026-06-05 18:48:36 +0530

    app.py
    hmm

--- app.py
+++ app.py
@@ -6,32 +6,22 @@ app = Flask(__name__)
 
 API_KEY = os.getenv("RAPIDAPI_KEY")
 
-@app.route("/")
-def home():
-    return "Instagram API is running!"
-
-@app.route("/instagram")
-def instagram():
-    username = request.args.get("username")
-
-    if not username:
-        return jsonify({
-            "error": "Please provide a username"
-        }), 400
+@app.route("/media")
+def media():
+    shortcode = request.args.get("shortcode")
 
     response = requests.get(
-        "https://instagram-scraper-api1.p.rapidapi.com/v2/user_info_by_username",
+        "https://instagram-scraper-ai1.p.rapidapi.com/media_id/from_shortcode/",
         headers={
             "x-rapidapi-key": API_KEY,
-            "x-rapidapi-host": "instagram-scraper-api1.p.rapidapi.com"
+            "x-rapidapi-host": "instagram-scraper-ai1.p.rapidapi.com",
+            "Content-Type": "application/json"
         },
-        params={"username": username}
+        params={"shortcode": shortcode}
     )
 
-    return jsonify({
-        "status_code": response.status_code,
-        "response": response.text
-    })
+    return jsonify(response.json())
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route("/")
+def home():
+    return "API is running!"
