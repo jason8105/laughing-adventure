cd2882c7d84b2fd66b69b9e9129224e8b757df58
jason8105 <jasondsilva8197@gmail.com>
2026-06-05 16:40:20 +0530

    Update app.py
    hmm

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Flask is working on Render!"
+API_KEY = os.getenv("RAPIDAPI_KEY")
+
+@app.route("/instagram")
+def instagram():
+    username = request.args.get("_jystn_dsilva_")
+
+    url = "https://instagram-scraper-api1.p.rapidapi.com/v2/user_info_by_username"
+
+    headers = {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "instagram-scraper-api1.p.rapidapi.com"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params={"username": username}
+    )
+
+    return jsonify(response.json())
